12d79863e8b779ed49f0dfee520062cec4edac14
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-20 20:15:30 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//Iniciando o Cronometro...
+Iniciar();
+
+
+
+
+static void Iniciar()
+{
+    int tempo = 10;
+    int tempoAtual = 0;
+
+    while (tempoAtual != tempo)
+    {
+        Console.Clear();
+       
+        tempoAtual++;
+        Console.WriteLine(tempoAtual);
+        Thread.Sleep(1000);
+    }
+}
